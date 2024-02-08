1118141ffa24117637fb2afaa4a3c7248ddf4e6e
Qartvela1 <qartvelishvilig340@gmail.com>
2024-02-08 16:48:20 +0400

    second commit

---
+++ day01/homework/lesson1.py
@@ -0,0 +1,11 @@
+
+name="giorgi"
+surname="qartvelishili"
+age="I'm 15 years old"
+height="1.50 metres"
+story="I entered the goal oriented academy because I wanted to learn a future profession such as programing"
+
+print(name + " " +  str(surname) + " " + str(age) + " " + str(height) + " " + str(story))
+
+
+
