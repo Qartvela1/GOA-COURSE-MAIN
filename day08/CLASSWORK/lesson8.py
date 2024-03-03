621c55b34d1ae9f17988904794607f8d49d1cd49
Qartvela1 <qartvelishvilig340@gmail.com>
2024-03-03 20:06:00 +0400

    commit

--- day08/CLASSWORK/lesson8.py
+++ day08/CLASSWORK/lesson8.py
@@ -1,8 +1,9 @@
-your_age = input("please enterd your age ")
+your_age = int(input("please enterd your age "))
 
 
-if your_age >=18:("you ar an adult")
-    
-if your_age <=18:("you ar not an adult")
+if your_age >=(18):
+      print("you are an adult")
+else:
+     print("You are not an adult")
 
 
---
+++ day08/HOMEWORK/lesson8.py
@@ -0,0 +1,10 @@
+name = input("what's your name: ")
+password = input("make your password: ")
+confrim_password = input("confrim your password:")
+
+repeat_password = password
+
+if password == confrim_password :
+    print("your password is correct")
+else:
+    print("your password is inncorect")
